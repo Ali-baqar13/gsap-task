9843415a25915a9e8b68cb1ccf59d2f939a3ae8d
Ali Baqar <baqar2502@gmail.com>
2026-04-28 17:34:55 +0500

    inittalization

---
+++ app/card-stack/page.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <main className="bg-foreground">
+      <section className="max-w-[1216px] mx-auto  py-[100px] ">
+        <div className="">
+          <h1 className="text-48-55 font-bold max-w-[550px] text-background">
+            Our Product{" "}
+            <span className="text-success w-full">Development Process</span>
+          </h1>
+          <div className="flex gap-[5px]"></div>
+        </div>
+      </section>
+    </main>
+  );
+}
+
+export default page
