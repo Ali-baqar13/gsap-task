f4fca7305ee415dd1a976fb1c325f86c48a03081
Ali Baqar <baqar2502@gmail.com>
2026-04-29 19:35:36 +0500

    work

--- app/card-stack/page.tsx
+++ app/card-stack/page.tsx
@@ -30,48 +30,84 @@ const data = [
   }
 ]
 const page = () => {
+  
+  useEffect(() => {
+    const cards = gsap.utils.toArray(".card");
+    gsap.registerPlugin(ScrollTrigger);
+
+   
+      const tl = gsap.timeline({
+        scrollTrigger: {
+          trigger: ".contain",
+          start: "top 20%",
+          end: "bottom 80%",
+          scrub: true,
+          pin: true,
+          markers: true,
+          pinSpacing: false,
+        },
+      });
+
+      // 1️⃣ bar animation
+      tl.to(".bar", {
+        scaleY: 1,
+        backgroundColor: "#E5E7EB",
+        transformOrigin: "top",
+        ease: "none",
+        scrollTrigger: {
+          trigger: ".contain",
+          start: "top 20%",
+          end: "bottom 80%",
+          scrub: true,
+          markers: true,
+          pinSpacing: false,
+        },
+      });
+    
+
+    
+  }, []);
 
  
 
- useEffect(() => {
-   gsap.registerPlugin(ScrollTrigger);
+//  useEffect(() => {
+//    gsap.registerPlugin(ScrollTrigger);
 
-   const cards = gsap.utils.toArray(".card");
+//    const cards = gsap.utils.toArray(".card");
 
-  //  console.log(cards, 'checking cards')
-
-  //  cards.forEach((card) => {
-     gsap.to(".contain", {
-       scrollTrigger: {
-         trigger: ".contain",
-         start: "top 20%",
-         end: "bottom 80%",
-         endTrigger: ".container",
-         pin: true,
-         scrub: true,
-         markers: true,
-         pinSpacing: false,
-       },
-     });
-   gsap.fromTo(
-     ".bar",
-     { y: 0 },
-     {
-       y:'-=100',
-       backgroundColor:'red',
-       ease: "none",
-       scrollTrigger: {
-         trigger: ".contain",
-         start: "top top",
-         end: "bottom top",
-         scrub: true,
-         markers: true,
-         pinSpacing: false,
-       },
-     },
-   );
-  //  });
- }, []);
+ 
+//      gsap.to(".contain", {
+//        scrollTrigger: {
+//          trigger: ".contain",
+//          start: "top 20%",
+//          end: "bottom 80%",
+//          endTrigger: ".container",
+//          pin: true,
+//          scrub: true,
+//          markers: true,
+//          pinSpacing: false,
+//        },
+//      });
+//    gsap.to(
+//      ".bar",
+    
+//      {
+//        scaleY: 1,
+//        backgroundColor: "#E5E7EB",
+//        transformOrigin: 'top',
+//        ease: "none",
+//        scrollTrigger: {
+//          trigger: ".contain",
+//          start: "top top",
+//          end: "bottom top",
+//          scrub: true,
+//          markers: true,
+//          pinSpacing: false,
+//        },
+//      },
+//    );
+//   //  });
+//  }, []);
     
       
 
@@ -79,16 +115,15 @@ const page = () => {
 
   return (
     <main className="bg-foreground">
-      <section className="container max-w-[1216px] mx-auto  py-[100px] ">
-        <div className="">
+      <section className="h-full container max-w-[1216px] mx-auto  py-[100px] ">
+        <div className="h-full">
           <h1 className="text-48-55 font-bold max-w-[550px] text-background mb-[80px]">
-            Our Product {' '}
+            Our Product{" "}
             <span className="text-success w-full">Development Process</span>
           </h1>
-          <div className="flex">
-            <div className=" mr-[48px]  relative  bg-progressBar  w-[4px] h-auto z-[7]">
-              <div className="bar absolute top-0 left-0 w-full h-full bg-white"></div>
-              
+          <div className="flex h-full">
+            <div className="mr-[48px] relative bg-progressBar w-[4px] h-[800px] z-[7] overflow-hidden">
+              <div className="bar w-full h-full  origin-top scale-y-0"></div>
             </div>
             <div className="flex flex-col">
               {data?.map((items, index) => (
